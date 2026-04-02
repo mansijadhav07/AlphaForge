e17c6af54ac66f66309ce5716bb68e0f776ba86f
Mansi Jadhav <mansikj07@gmail.com>
2026-04-02 10:30:43 +0530

    navbar update

--- frontend/components/ui/fullscreen-loader.tsx
+++ frontend/components/ui/fullscreen-loader.tsx
@@ -20,7 +20,7 @@ export function FullScreenLoader({
   }, [])
 
   return (
-    <div className="fixed inset-0 z-40 flex items-center justify-center bg-[#0b0f17]" style={{ paddingTop: '64px' }}>
+    <div className="fixed top-16 left-0 right-0 bottom-0 z-40 flex items-center justify-center bg-[#0b0f17]">
       {/* Subtle radial glow in background */}
       <div 
         className="absolute inset-0 opacity-30"
